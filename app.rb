5a37a60c28f0f83a1fc56c8bfde93aa0e1de9040
themoonofendor <evoskoley@kespry.com>
2017-08-03 15:16:55 -0700

    add app.rb and view/index.erb

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "postgresql:pizzashop.db"
+
+class Product < ActiveRecord::Base
+end
+
+get '/' do
+	@products = Product.all
+	erb :index
+end
+
+get '/about' do
+	erb :about
+end
