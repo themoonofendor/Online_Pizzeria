8eed17a37560a4c5b8c19a969dee845921380b93
themoonofendor <evoskoley@kespry.com>
2017-08-04 12:15:28 -0700

    another db migration

---
+++ db/migrate/20170804185439_add_products.rb
@@ -0,0 +1,37 @@
+class AddProducts < ActiveRecord::Migration[5.1]
+  def change
+
+  	Product.create ({
+  		:title => 'Hawaiian',
+  		:description => 'This is Hawaiian pizza',
+  		:price => 15,
+  		:size => 14,
+  		:is_spicy => false,
+  		:is_veg => false,
+  		:is_best_offer => false,
+  		:path_to_image => '/images/hawaiian.jpg'
+  		})
+
+  	Product.create ({
+  		:title => 'Pepperoni',
+  		:description => 'Nice Pepperoni pizza',
+  		:price => 10,
+  		:size => 14,
+  		:is_spicy => false,
+  		:is_veg => false,
+  		:is_best_offer => true,
+  		:path_to_image => '/images/pepperoni.jpg'
+  		})
+
+  	Product.create ({
+  		:title => 'Vegetarian',
+  		:description => 'Amazing Vegetarian pizza',
+  		:price => 18,
+  		:size => 14,
+  		:is_spicy => false,
+  		:is_veg => false,
+  		:is_best_offer => false,
+  		:path_to_image => '/images/veg.jpg'
+  		})
+  end
+end
