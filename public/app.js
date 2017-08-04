8672ea7bdff868200eeb76624b70c080ebe44bf6
themoonofendor <evoskoley@kespry.com>
2017-08-04 12:19:41 -0700

    output of number of items in the cart

--- public/app.js
+++ public/app.js
@@ -7,14 +7,25 @@ function something()
 
 	window.localStorage.setItem('bbb', x);
 
-	alert(x);
+	// output of number of items in the cart
+	alert('Items in your cart: ' + cart_get_number_of_items());
 }
 
-function add_to_cart(id)
+function cart_get_number_of_items()
 {
-	var key = 'product_' + id;
+	var cnt = 0;
 
-	var x = window.localStorage.getItem(key);
-	x = x * 1 + 1;
-	window.localStorage.setItem(key, x);
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
 }
+Contact GitHub API Training Shop Blog About
