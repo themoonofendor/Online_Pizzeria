c44df037f8d453d4570d972ffc28177876f3b8aa
themoonofendor <evoskoley@kespry.com>
2017-08-10 17:23:10 -0700

    Revert "polish app.rb"yes
    This reverts commit e0bb8f0cff3ef1b7a10ac66735c0bcc31157d63a.

--- app.rb
+++ app.rb
@@ -3,17 +3,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :bind, '0.0.0.0'
-set :database, "sqlite3:pizzashop.db"
+set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'onlinepizzeria'}
 
 class Product < ActiveRecord::Base
 end
 
-class Shipment < ActiveRecord::Base
-  validates :scope, presence: true
-  validates :name, presence: true
-  validates :phone, presence: true
-  validates :address, presence: true
+class Order < ActiveRecord::Base
 end
 
 get '/' do
@@ -25,60 +20,51 @@ get '/about' do
 	erb :about
 end
 
-get '/contacts' do
-  erb "Under construction"
-end
+post '/place_order' do
+   @order = Order.create params[:order]
+   erb :order_placed
+ end
 
 post '/cart' do
-  @orders_input = params[:orderstring]
+
+	# getting the list of parameters parse them
+
+  @orders_input = params[:orders_input]
   @items = parse_orders_input @orders_input
 
-  # выводим сообщение о том, что корзина пуста
-  if @items.length == 0
-    return erb :cart_is_empty
-  end
+  # output message that cart is empty
+ 
+   if @items.length == 0
+     return erb :cart_is_empty
+   end
+ 
+   # output list of items in the cart
 
   @items.each do |item|
     # id, cnt
     item[0] = Product.find(item[0])
   end
-
+  # return default
 	erb :cart
-end
+ end
 
-get '/admin' do
-   @shipms = Shipment.all
-  erb :orders_list
-end
-
-post '/place_order' do
-  @shipment = Shipment.new params[:order]
-
-  if @shipment.save
-    erb :order_placed
-  else
-    @error = @shipment.errors.full_messages.first
-    erb "Error"
-  end
-end
-
-def parse_orders_input orders_input
-  s1 = orders_input.split(/,/)
-
-  arr = []
-
-  s1.each do |x|
-    s2 = x.split(/\=/)
-
-    s3 = s2[0].split(/_/)
-
-    id = s3[1]
-    cnt = s2[1]
-
-    arr2 = [id, cnt]
-
-    arr.push arr2
-  end
-
-  return arr
+ def parse_orders_input orders_input
+   s1 = orders_input.split(/,/)
+ 
+   arr = []
+ 
+   s1.each do |x|
+     s2 = x.split(/\=/)
+ 
+     s3 = s2[0].split(/_/)
+ 
+     id = s3[1]
+     cnt = s2[1]
+ 
+     arr2 = [id, cnt]
+ 
+     arr.push arr2
+   end
+ 
+   return arr
 end
