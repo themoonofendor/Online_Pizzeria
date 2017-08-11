f84fd5d20bd7bb8fa1041f48bfdd0e00769b8e65
themoonofendor <evoskoley@kespry.com>
2017-08-10 17:28:37 -0700

    polish app.rb more

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'onlinepi
 class Product < ActiveRecord::Base
 end
 
-class Order < ActiveRecord::Base
+class Shipment < ActiveRecord::Base
+  validates :scope, presence: true
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
 end
 
 get '/' do
@@ -20,51 +24,60 @@ get '/about' do
 	erb :about
 end
 
-post '/place_order' do
-   @order = Order.create params[:order]
-   erb :order_placed
- end
+get '/contacts' do
+  erb "Under construction"
+end
 
 post '/cart' do
-
-	# getting the list of parameters parse them
-
-  @orders_input = params[:orders_input]
+  @orders_input = params[:orderstring]
   @items = parse_orders_input @orders_input
 
-  # output message that cart is empty
- 
-   if @items.length == 0
-     return erb :cart_is_empty
-   end
- 
-   # output list of items in the cart
+  # выводим сообщение о том, что корзина пуста
+  if @items.length == 0
+    return erb :cart_is_empty
+  end
 
   @items.each do |item|
     # id, cnt
     item[0] = Product.find(item[0])
   end
-  # return default
-	erb :cart
- end
 
- def parse_orders_input orders_input
-   s1 = orders_input.split(/,/)
- 
-   arr = []
- 
-   s1.each do |x|
-     s2 = x.split(/\=/)
- 
-     s3 = s2[0].split(/_/)
- 
-     id = s3[1]
-     cnt = s2[1]
- 
-     arr2 = [id, cnt]
- 
-     arr.push arr2
-   end
- 
-   return arr
+	erb :cart
+end
+
+get '/admin' do
+   @shipms = Shipment.all
+  erb :orders_list
+end
+
+post '/place_order' do
+  @shipment = Shipment.new params[:order]
+
+  if @shipment.save
+    erb :order_placed
+  else
+    @error = @shipment.errors.full_messages.first
+    erb "Error"
+  end
+end
+
+def parse_orders_input orders_input
+  s1 = orders_input.split(/,/)
+
+  arr = []
+
+  s1.each do |x|
+    s2 = x.split(/\=/)
+
+    s3 = s2[0].split(/_/)
+
+    id = s3[1]
+    cnt = s2[1]
+
+    arr2 = [id, cnt]
+
+    arr.push arr2
+  end
+
+  return arr
 end
